9a5d18ef42b60978ac0ddca2760887f20b788ed9
Lucas Kranz <kranz12@gmail.com>
2023-02-13 16:25:11 -0300

    Arquitetura da API de Catalogo, Modelagem, Acesso a Dados.

--- src/services/NSE.Catalogo.API/Program.cs
+++ src/services/NSE.Catalogo.API/Program.cs
@@ -1,15 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Data;
+using NSE.Catalogo.API.Data.Repository;
+using NSE.Catalogo.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<CatalogoContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<CatalogoContext>();  
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
